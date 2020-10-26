92db1aacafab8f4a09b55bbf6b120104269a2f0a
Joseph <josephvarghese.rms@gmail.com>
2020-10-27 01:56:36 +0530

    Added route to delete task

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::TasksController < ApplicationController
-  before_action :load_task, only: [:show, :update]
+  before_action :load_task, only: [:show, :update, :destroy]
 
   def index
     @tasks = Task.all
@@ -25,6 +25,14 @@ class Api::V1::TasksController < ApplicationController
     else
       errors = @task.errors.full_messages
       render status: :unprocessable_entity, json: { errors: errors  }
+    end
+  end
+
+  def destroy
+    if @task.destroy
+      render status: :ok, json: { notice: "Successfully deleted task." }
+    else
+      render status: :unprocessable_entity, json: { errors: @task.errors.full_messages }
     end
   end
 
--- config/routes/api.rb
+++ config/routes/api.rb
@@ -1,5 +1,5 @@
 namespace :api do
   namespace :v1 do
-    resources :tasks, only: [:index, :create, :show, :update]
+    resources :tasks, only: [:index, :create, :show, :update, :destroy]
   end
 end
