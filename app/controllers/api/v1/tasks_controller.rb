8298d37901109736698040fbf15dbf8fd01b4db4
Joseph <josephvarghese.rms@gmail.com>
2020-10-27 01:30:25 +0530

    Created route to create tasks

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -2,4 +2,21 @@ class Api::V1::TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      render status: :ok, json: { notice: 'Task was successfully created' }
+    else
+      errors = @task.errors.full_messages
+      render status: :unprocessable_entity, json: { errors: errors  }
+    end
+  end
+
+  private
+
+  def task_params
+    params.permit(:description)
+  end
 end
--- config/routes/api.rb
+++ config/routes/api.rb
@@ -1,5 +1,5 @@
 namespace :api do
   namespace :v1 do
-    resources :tasks, only: :index
+    resources :tasks, only: [:index, :create]
   end
 end
