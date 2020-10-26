eb150ceaa512b4ee824ba684ce80ff1d2ab0a5d3
Joseph <josephvarghese.rms@gmail.com>
2020-10-27 00:11:04 +0530

    Generated tasks controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_12_073702) do
+ActiveRecord::Schema.define(version: 2020_10_26_181903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
