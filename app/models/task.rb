16782b2c322722481fbad6f99e35f7fa22a5df53
Joseph <josephvarghese.rms@gmail.com>
2020-10-27 22:28:40 +0530

    Commented background worker tasks in task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,9 +2,9 @@ class Task < ApplicationRecord
   validates :description, presence: true
   belongs_to :user, optional: true
   has_many :comments, dependent: :destroy
-  after_create :log_task_details
+  # after_create :log_task_details
 
-  def log_task_details
-    TaskLoggerJob.perform_later(self)
-  end
+  # def log_task_details
+  #   TaskLoggerJob.perform_later(self)
+  # end
 end
