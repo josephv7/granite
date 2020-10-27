7074ad5b3e972b4b45c63b482e439cc77f1c5ee5
Joseph <josephvarghese.rms@gmail.com>
2020-10-27 08:56:51 +0530

    Added basic logic for delete

--- app/javascript/apis/tasks.js
+++ app/javascript/apis/tasks.js
@@ -3,9 +3,11 @@ import axios from "axios";
 const listTasks = () => axios.get("/api/v1/tasks");
 const createTask = (payload) => axios.post("/api/v1/tasks", payload)
 const updateTask = (payload) => axios.put("/api/v1/tasks/3", payload)
+const deleteTask = (id) => axios.delete(`/api/v1/tasks/${id}`)
 
 export default {
   listTasks,
   createTask,
-  updateTask
+  updateTask,
+  deleteTask
 };
--- app/javascript/components/TaskList.js
+++ app/javascript/components/TaskList.js
@@ -17,6 +17,19 @@ const TaskList = () => {
     }
   };
 
+  const deleteTask = async (id) => {
+    try{
+      const response = await tasksAPI.deleteTask(id)
+      console.log(response)
+    }catch(error){
+      console.log(error)
+      if(error.response.status == 403) 
+        console.log("No permission")
+    }finally{
+
+    }
+  }
+
   useEffect(() => {
     fetchTasks();
   }, []);
@@ -26,43 +39,57 @@ const TaskList = () => {
       <Card>
         {tasks.map((item, index) => {
           return (
-            <div
-              className="d-flex flex-row justify-content-around align-items-center w-100 flex-wrap"
-              key={index}
-            >
-              <button
-                type="button"
-                className="btn btn-light w-25 my-1"
-                disabled
-              >
-                {item.id}
-              </button>
-              <button
-                type="button"
-                className="btn btn-light w-25 my-1"
-                disabled
-              >
-                {item.description}
-              </button>
-              {/* <button type="button" className=" btn btn-light w-10" disabled>
-                {item.count}
-              </button>
-              <button
-                type="button"
-                className=" btn btn-light w-10"
-                onClick={() => showLogs(item.id)}
-              >
-                View Logs
-              </button> */}
+            // <div
+            //   className="d-flex flex-row justify-content-around align-items-center w-100 flex-wrap"
+            //   key={index}
+            // >
+            //   <button
+            //     type="button"
+            //     className="btn btn-light w-25 my-1"
+            //     disabled
+            //   >
+            //     {item.id}
+            //   </button>
+            //   <button
+            //     type="button"
+            //     className="btn btn-light w-25 my-1"
+            //     disabled
+            //   >
+            //     {item.description}
+            //   </button>
+            //   {/* <button type="button" className=" btn btn-light w-10" disabled>
+            //     {item.count}
+            //   </button>
+            //   <button
+            //     type="button"
+            //     className=" btn btn-light w-10"
+            //     onClick={() => showLogs(item.id)}
+            //   >
+            //     View Logs
+            //   </button> */}
+            // </div>
+
+            <div className="card" style={{width: "18rem"}} key={index}>
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                {/* <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6> */}
+                <p className="card-text">
+                  {item.description}
+                </p>
+                <a href="#" className="card-link">
+                  Update
+                </a>
+                <a className="card-link" onClick={()=> deleteTask(item.id)}>
+                  Delete
+                </a>
+              </div>
             </div>
           );
         })}
       </Card>
     );
-  }else {
-    return (
-      <h1>No Tasks</h1>
-    )
+  } else {
+    return <h1>No Tasks</h1>;
   }
 };
 
